343cca6471b9514f751c8d065e186bffc36f49a6
yubraj <yubraj2000yubraj@gmail.com>
2026-08-29 16:29:15 +0530

    "uid-1: Remove unused CSS styles and components; add Header component and variables.css"

--- notes-gen-ui/src/main.tsx
+++ notes-gen-ui/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// importing styles
+import './styles/variables.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
